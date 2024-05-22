b68c1fa3c50d76f243195005b7b858c3fd90b417
Vadim Pryadilshchikov <vadim-pr2001@mail.ru>
2024-05-22 11:27:51 +0300

    init all points with green

--- scene/gaussian_model.py
+++ scene/gaussian_model.py
@@ -124,7 +124,11 @@ class GaussianModel:
     def create_from_pcd(self, pcd : BasicPointCloud, spatial_lr_scale : float):
         self.spatial_lr_scale = spatial_lr_scale
         fused_point_cloud = torch.tensor(np.asarray(pcd.points)).float().cuda()
-        fused_color = RGB2SH(torch.tensor(np.asarray(pcd.colors)).float().cuda())
+
+        init_color = torch.tensor([0, 1, 0]) #green
+        init_colors = torch.tile(init_color, (len(pcd.colors), 1)).float().cuda()
+
+        fused_color = RGB2SH(init_colors)
         features = torch.zeros((fused_color.shape[0], 3, (self.max_sh_degree + 1) ** 2)).float().cuda()
         features[:, :3, 0 ] = fused_color
         features[:, 3:, 1:] = 0.0
